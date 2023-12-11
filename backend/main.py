5ffdff849becb640c52a8606bff60db377b8e3a9
Kacper_M <74098084+Kacper-017@users.noreply.github.com>
2023-12-11 11:04:03 +0100

    Added chat functionality

--- backend/main.py
+++ backend/main.py
@@ -10,12 +10,30 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.post("/sendMessage")
-async def sendMessage(request: Request):
-    try:
-        data = await request.json()  
-        print('body', data)
-        return 'Hello'
-    except Exception as e:
-        return {"error": str(e)}
+@app.post("/chat")
+async def chat_with_gpt(request: ChatRequest):
+    # Join all the messages into a single string
+    chat_input = "\n".join(request.messages)
 
+    # Call the OpenAI API to get the chat response
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers={
+                "Authorization": "Bearer YOUR_OPENAI_API_KEY",
+                "Content-Type": "application/json"
+            },
+            json={
+                "messages": [{"role": "system", "content": "You finish user's sentence."}, {"role": "user", "content": chat_input}],
+                "model": "gpt-3.5-turbo",
+                "max_tokens": 50,
+                "temperature": 0.7,
+                "n": 1,
+                "stop": None
+            }
+        )
+
+    # Extract the generated message from the API response
+    chat_output = response.json()["choices"][0]["message"]["content"].strip()
+
+    return ChatResponse(message=chat_output)
